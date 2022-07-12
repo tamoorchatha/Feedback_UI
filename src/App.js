e2d07758d13b65eda8b14009bce068581d22fc70
tamoorchatha <tamoorchatha82@gmail.com>
2022-07-12 14:25:54 +0500

    added stats about reviews dynamically

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
 export default function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
-  function deleteFeedback(id){
-    setFeedback(feedback.filter((item)=>{
-      return item.id !== id
-    }))
+  function deleteFeedback(id) {
+    if (window.confirm("ARE YOU SURE? DO YOU WANT TO DELETE IT?")) {
+      setFeedback(
+        feedback.filter((item) => {
+          return item.id !== id;
+        })
+      );
+    }
   }
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+        <FeedbackStats feedback={feedback}/>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
